aafc364e9fc211927d5d63a7697152f58b88d81f
Sachin Chandy <sachinmchandy@gmail.com>
2017-07-23 09:05:49 +0100

    non_OOP_cw

--- hungerGamesZoo.js
+++ hungerGamesZoo.js
@@ -1,124 +1,55 @@
+var whoEatsWho = function(zoo) {
 
+var animals = ["antelope eats grass",
+"big-fish eats little-fish",
+"bug eats leaves",
+"bear eats big-fish",
+"bear eats bug",
+"bear eats chicken",
+"bear eats cow",
+"bear eats leaves",
+"bear eats sheep",
+"chicken eats bug",
+"cow eats grass",
+"fox eats chicken",
+"fox eats sheep",
+"giraffe eats leaves",
+"lion eats antelope",
+"lion eats cow",
+"panda eats leaves",
+"sheep eats grass"];
 
-
-
-
-
-//var animalEats = ["antelope eats grass", "big-fish eats little-fish", "bug eats leaves", bear eats big-fish, bear eats bug, bear eats chicken,
-//bear eats cow, bear eats leaves, bear eats sheep, chicken eats bug, cow eats grass, fox eats chicken, fox eats sheep, giraffe eats leaves,
-//lion eats antelope, lion eats cow, panda eats leaves, sheep eats grass"];
-
-/*
-antelope eats grass
-big-fish eats little-fish
-bug eats leaves
-bear eats big-fish
-bear eats bug
-bear eats chicken
-bear eats cow
-bear eats leaves
-bear eats sheep
-chicken eats bug
-cow eats grass
-fox eats chicken
-fox eats sheep
-giraffe eats leaves
-lion eats antelope
-lion eats cow
-panda eats leaves
-sheep eats grass
-
-var fox = {
-  chicken: 'fox eats chicken',
-  sheep: 'fox eats sheep'
-};
-
-var panda = {
-  leaves: 'panda eats leaves'
-}
-
-var zooDisaster = {
-  fox: {chicken: 'fox eats chicken', sheep: 'fox eats sheep'},
-  panda: {leaves: 'panda eats leaves'}
-}
-
-*/
-
-/*
-iterate through input - for loop
-check if obj to the left and right are properties of the calling object
-if obj to the left/right is a property (e.g chicken)
-  remove that obj from the input array
-  return the value of that property to the final array (e.g 'fox eats chicken')
-  move the search counter back to the start of the input (reset counter)
-*/
-
-var input = "fox,bug,chicken,grass,sheep";
+var input = zoo;
 input = input.split(',');
-var finalArray = ["fox,bug,chicken,grass,sheep"];
-var counter = 0;
-/*
-var fox = {
-  chicken: 'fox eats chicken',
-  sheep: 'fox eats sheep'
-};
+var finalArray = [];
 
-var bug = {};
+var str = "";
 
-var chicken = {
-  bug: 'chicken eats bug'
-};
-
-var grass = {};
-
-var sheep = {
-  grass: 'sheep easts grass'
-}
-*/
-var animals = {
-  bug: {bug: {}},
-  grass: {grass: {}},
-  chicken: {chicken: {bug: 'chicken eats bug'}},
-  sheep: {sheep: {grass: 'sheep eats grass'}},
-  fox: {fox: {chicken: 'fox eats chicken', sheep: 'fox eats sheep'}}
-}
-
-var arr = input.map(function(element) {
-  if (animals.hasOwnProperty(element))
-    return animals[element];
-});
-
-
-arr.filter(function(element, index) {
-  if (element.hasOwnProperty(Object.keys(element).shift()))
-    return element[Object.keys(element).shift()];
-});
-
-[ { fox: { chicken: 'fox eats chicken', sheep: 'fox eats sheep' } },
-  { bug: {} },
-  { chicken: { bug: 'chicken eats bug' } },
-  { grass: {} },
-  { sheep: { grass: 'sheep eats grass' } } ]
-
-
-/*
 for(var i = 0; i < input.length; i++) {
   if(input[i-1] != undefined) {
-    debugger;
-    if (input[i].hasOwnProperty(input[i-1])) {
-      finalArray.push(input[i][input[i-1]]);
+    str = input[i] + " eats " + input[i-1];
+    if (animals.indexOf(str) != -1) {
+      finalArray.push(str);
       input.splice(i-1,1);
       i = -1;
+      continue;
     }
-  } else if (input[i+1] != undefined) {
-      debugger;
-      if (input[i].hasOwnProperty(input[i+1])) {
-        finalArray.push(input[i][input[i+1]]);
-        input.splice(i+1,1);
-        i = -1;
-      }
+  }
+
+ if(input[i+1] != undefined) {
+   str = input[i] + " eats " + input[i+1];
+   if(animals.indexOf(str) != -1) {
+     finalArray.push(str);
+     input.splice(i+1,1);
+     i = -1;
+     continue;
+    }
   }
 }
 
-console.log(finalArray);
-*/
+finalArray.push(input.join());
+finalArray.unshift(zoo);
+
+return finalArray;
+  
+}
