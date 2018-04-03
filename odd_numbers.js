2239cd600bc875d75ec8b38a4f94fac506068d76
Sachin Chandy <sachinmchandy@gmail.com>
2018-04-03 19:15:57 +0300

    odd numbers

--- odd_numbers.js
+++ odd_numbers.js
@@ -1,7 +1,34 @@
+/*
 function logOddNumbers(num) {
   for (var i = 0; i <= num; i++) {
     if (i % 2 != 0) console.log(i);
   }
 }
+*/
+
+/*
+function logOddNumbers(num) {
+  for (var i = 2; ; i += 2) {
+    console.log(i - 1);
+    if (i > num) break;
+  }
+}
+*/
+
+/*
+function logOddNumbers(num) {
+  for (var i = 1; i <= num; i += 2) {
+    console.log(i);
+  }
+}
+*/
+
+function logOddNumbers(num) {
+  for (var i = 0; i <= num; i++) {
+    if (i % 2 == 0) continue;
+    console.log(i);
+  }
+}
+
 
 logOddNumbers(19);
