2e9b0ceda8f588a5b60e57907d46ffc283a92e44
Sachin Chandy <sachinmchandy@gmail.com>
2018-04-11 07:13:37 +0300

    problems

---
+++ array-concat-part2.js
@@ -0,0 +1,21 @@
+function concat() {
+  var i;
+  var j;
+  var result = [];
+
+  for (i = 0; i < arguments.length; i += 1) {
+    if (Array.isArray(arguments[i])) {
+      for (j = 0; j < arguments[i].length; j += 1) {
+        result.push(arguments[i][j]);
+      }
+    } else {
+      result.push(arguments[i]);
+    }
+  }
+
+  return result;
+}
+
+console.log(concat([1, 2, 3], [4, 5, 6], [7, 8, 9]));    // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(concat([1, 2], 'a', ['one', 'two']));        // [1, 2, "a", "one", "two"]
+console.log(concat([1, 2], ['three'], 4));               // [1, 2, "three", 4]
