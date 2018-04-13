2ac9b639a66bf0e723611bcd35d02ba9356da00d
Sachin Chandy <sachinmchandy@gmail.com>
2018-04-13 09:25:58 +0300

    problems

---
+++ after-midnight-part2.js
@@ -0,0 +1,44 @@
+
+var MINUTES_PER_HOUR = 60;
+var HOURS_PER_DAY = 24;
+var MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR;
+
+function afterMidnight(timeStr) {
+  var timeComponents = timeStr.split(':');
+  var hours = parseInt(timeComponents[0], 10);
+  var minutes = parseInt(timeComponents[1], 10);
+
+  return hours * MINUTES_PER_HOUR + minutes;
+}
+
+function beforeMidnight(timeStr) {
+  var deltaMinutes = MINUTES_PER_DAY - afterMidnight(timeStr);
+  if (deltaMinutes === MINUTES_PER_DAY) {
+    deltaMinutes = 0;
+  }
+
+  return deltaMinutes;
+}
+
+//refactor with Date object
+
+function afterMidnight(timeStr) {
+  var dateObj = new Date('1/1/2000 ' + timeStr);
+  return dateObj.getHours() * MINUTES_PER_HOUR + dateObj.getMinutes();
+}
+
+function beforeMidnight(timeStr) {
+  var deltaMinutes = MINUTES_PER_DAY - afterMidnight(timeStr);
+  if (deltaMinutes === MINUTES_PER_DAY) {
+    deltaMinutes = 0;
+  }
+
+  return deltaMinutes;
+}
+
+
+
+console.log(afterMidnight('00:00'));       // 0
+console.log(beforeMidnight('00:00'));      // 0
+console.log(afterMidnight('12:34'));       // 754
+console.log(beforeMidnight('12:34'));      // 686
