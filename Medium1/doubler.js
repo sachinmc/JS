38911bc8dc6da7b39caf94f315b8168981fcc5b2
Sachin Chandy <sachinmchandy@gmail.com>
2018-04-16 14:08:55 +0300

    closure, function returning a function

---
+++ Medium1/doubler.js
@@ -0,0 +1,9 @@
+function makeDoubler(caller) {
+  return function(number) {
+    console.log('This function was called by ' + caller + '.');
+    return number + number;
+  }
+}
+
+var doubler = makeDoubler('Victor');
+doubler(5);
